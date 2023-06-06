72cc368a45023720c6c54b208100b344f6737d87
anazworth <98629900+anazworth@users.noreply.github.com>
2023-06-06 00:00:33 -0400

    Added NGINX as loadbalancer layer

--- auth_server/auth-service/src/main/java/com/anazworth/authservice/user/UserController.java
+++ auth_server/auth-service/src/main/java/com/anazworth/authservice/user/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.anazworth.authservice.jwt.JwtUtils;
 
 
 @RestController
+@RequestMapping("/api/v1")
 @ComponentScan("com.anazworth.authservice")
 public class UserController {
     
